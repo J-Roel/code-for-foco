cd73fbbcd44473d02390cfae1ad033d3c450d75b
Jeremy Roelfs <jeremyroelfs@icloud.com>
2016-04-18 17:24:27 -0700

    Fixed header with clear change to blue and fixed, autoscroll not working

--- js/autoscroll.js
+++ js/autoscroll.js
@@ -1,7 +1,5 @@
-        
 
-/*REQUIRES JQUERY*/
-	$(function() {
+  $(function() {
           $('a[href*="#"]:not([href="#"])').click(function() {
             if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
               var target = $(this.hash);
@@ -15,8 +13,3 @@
             }
         });
     });
-
-	setTimeout( function(){
-    $('#main-menu').attr('checked','true');
-       },3000);
-
